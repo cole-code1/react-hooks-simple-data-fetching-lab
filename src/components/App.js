0e3f5160c3720df1312bb74a913bd9816078480d
Collins Kathurima <collinskathu313@gmail.com>
2024-11-09 15:12:56 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import { useEffect,useState } from "react";
+
+function App(){
+const [image, setImage] = useState(null)
+
+useEffect(()=>{
+    fetch ("https://dog.ceo/api/breeds/image/random")
+        .then ((response)=> response.json())
+        .then ((data)=>{setImage(data.message)});
+        
+},[]);
+
+if (!image){
+    return <h2>Loading...</h2>
+}
+return(
+    <div>
+        <img src={image} alt="A Random Dog"></img>
+    </div>
+)
+}
+export default App;
